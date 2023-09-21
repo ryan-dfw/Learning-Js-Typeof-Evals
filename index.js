f4c578ac6ff554948a5721f06a99ea78b923cdbc
gdknas <mckevitt.r@gmail.com>
2023-09-20 23:17:01 -0500

    expressed test cases as array

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function evaluate(value) {
-  console.log(value ? !!value : "the " + typeof value + " is falsy");
-}
+const evaluate = (values) =>
+  values.forEach((value) =>
+    console.log(value ? !!value : `the ${typeof value} is falsy`)
+  );
 
-evaluate("string");
-evaluate(false);
-evaluate(null);
-evaluate(undefined);
-evaluate(0);
-evaluate("");
+evaluate(["string", false, null, undefined, 0, ""]);
