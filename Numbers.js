daec3708354db7e3dfba8a5109986c77d8e94672
gdknas <mckevitt.r@gmail.com>
2023-09-22 16:03:55 -0500

    created in Numbers for all six cases

--- Numbers.js
+++ Numbers.js
@@ -1,13 +1,33 @@
-const num1 = 50;
-const num2 = 51;
-const target = 100;
+const valuesArrayA = [
+  [50, 51, 100],
+  [99, -2, 0],
+  [0, 101, 100],
+  [500, -500, 0],
+];
 
-function compEval(num1, num2, target) {
-  return num1 + num2 > target
+const valuesArrayB = [
+  [-1000, 0],
+  [-5, 0],
+];
+
+const compEval = (num1, num2, target) =>
+  num1 + num2 > target
     ? "greater than"
     : num1 + num2 === target
     ? "equal to"
     : "less than";
-}
 
-console.log(`${num1} + ${num2} is ${compEval(num1, num2, target)} ${target}`);
+const signTest = (num1, num2) =>
+  num1 + num2 < 0
+    ? "a negative number"
+    : num1 + num2 === 0
+    ? "0"
+    : "a positive number";
+
+valuesArrayA.forEach(([num1, num2, target]) =>
+  console.log(`${num1 + num2} is ${compEval(num1, num2, target)} ${target}`)
+);
+
+valuesArrayB.forEach(([num1, num2]) =>
+  console.log(`${num1 + num2} is ${signTest(num1, num2)}`)
+);
