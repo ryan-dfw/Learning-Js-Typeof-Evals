6a6ad1aa7754cbe003ff8364a991c8bdb35702d3
gdknas <mckevitt.r@gmail.com>
2023-09-22 15:46:38 -0500

    simplified Numbers to call compEval directly in console.log

--- Numbers.js
+++ Numbers.js
@@ -3,15 +3,11 @@ const num2 = 51;
 const target = 100;
 
 function compEval(num1, num2, target) {
-  const result =
-    num1 + num2 > target
-      ? "greater than"
-      : num1 + num2 === target
-      ? "equal to"
-      : "less than";
-  return result;
+  return num1 + num2 > target
+    ? "greater than"
+    : num1 + num2 === target
+    ? "equal to"
+    : "less than";
 }
 
-const result = compEval(num1, num2, target);
-
-console.log(`${num1 + num2} is ${result} ${target}`);
+console.log(`${num1} + ${num2} is ${compEval(num1, num2, target)} ${target}`);
